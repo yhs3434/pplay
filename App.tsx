b8f819f02fcc69f053fb5f5c4b47ee574f486a6f
Hansol Yoon <hansol@Hansols-MacBook-Pro.local>
2023-02-26 16:45:39 +0900

    chore: react navigation init.

--- App.tsx
+++ App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import type {PropsWithChildren} from 'react';
-import {SafeAreaView, View, Text, StatusBar} from 'react-native';
+import {SafeAreaView, Text, StatusBar} from 'react-native';
 import styled from 'styled-components';
+import {NavigationContainer} from '@react-navigation/native';
 
 type Props = PropsWithChildren<{
   //
@@ -9,11 +10,21 @@ type Props = PropsWithChildren<{
 
 export default function App(props: Props): JSX.Element {
   return (
-    <Container>
-      <StatusBar />
-      <Text>traverse</Text>
-    </Container>
+    <Wrapper>
+      <Container>
+        <StatusBar />
+        <Text>traverse</Text>
+      </Container>
+    </Wrapper>
   );
 }
 
 const Container = styled(SafeAreaView)``;
+
+type WrapperProps = {
+  children: React.ReactNode;
+};
+
+const Wrapper = ({children}: WrapperProps) => {
+  return <NavigationContainer>{children}</NavigationContainer>;
+};
