0500cdbd6d283f502499be739cc5ae3e13913eeb
Hansol Yoon <hansol@Hansols-MacBook-Pro.local>
2023-03-07 23:49:36 +0900

    chore: ios app name change.

--- ios/pplayTests/pplayTests.m
+++ ios/pplayTests/pplayTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface ppleTests : XCTestCase
+@interface pplayTests : XCTestCase
 
 @end
 
-@implementation ppleTests
+@implementation pplayTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
